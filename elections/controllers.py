7ecd1eea554a4c10a9a722591b87533c5db899ac
Nigel Jones <dev@nigelj.com>
2008-04-24 22:34:07 +0000

    Controllers: * index() only displays elections with an id > 0 * info() now handles 'shortnames' Templates: * list.html now handles 'shortnames'
    git-svn-id: svn+ssh://svn.fedorahosted.org/svn/elections/trunk@6 d77cda62-7b1a-441a-8d08-c06ad5917f56

--- elections/controllers.py
+++ elections/controllers.py
@@ -14,13 +14,20 @@ class Root(controllers.RootController):
     admin = Admin()
     @expose(template="elections.templates.list")
     def index(self):
-        electlist = Elections.query.order_by(ElectionsTable.c.start_date).all()
+        electlist = Elections.query.order_by(ElectionsTable.c.start_date).filter('id>0').all()
         return dict(elections=electlist, currenttime=datetime.utcnow())
+
+
     @expose(template="elections.templates.info")
     def info(self,eid=None):
-        if eid:
-            candidates = Candidates.query.filter_by(election_id=eid).all()
-            return dict(eid=eid, candidates=candidates)
+        try:
+            eid = int(eid)
+        except ValueError:
+            eid = Elections.query.filter_by(shortname=eid).all()[0].id
+        candidates = Candidates.query.filter_by(election_id=eid).all()
+        return dict(eid=eid, candidates=candidates)
+
+
     @expose()
     def vote(self, cid, **kw):        
         if "weight" in kw and "name" in kw:
