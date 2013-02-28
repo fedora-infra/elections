c3cca1ae85972a16fa9f63357ecb38c066ea8427
fchiulli <fchiulli@elections-dev-i-57734127.(none)>
2013-02-27 22:35:59 -0500

    Update documentation

---
+++ setup.py
@@ -0,0 +1,16 @@
+## These two lines are needed to run on EL6
+__requires__ = ['SQLAlchemy >= 0.7', 'jinja2 >= 2.4']
+import pkg_resources
+
+
+from setuptools import setup, find_packages
+
+setup( name = 'fedora-elections'
+     , version = '@VERSION@'
+     , author = 'Frank Chiulli'
+     , author_email = 'fchiulli@fedoraproject.org'
+     , packages = find_packages()
+     , zip_safe = False
+     , include_package_data = True
+     , install_requires = ['Flask', 'SQLAlchemy>=0.7', 'flask-sqlalchemy', 'beaker']
+     )
