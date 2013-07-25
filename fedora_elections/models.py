b1e311aa6811e97337f9949865afada0137e453e
Frank Chiulli <fchiulli@fedoraproject.org>
2013-07-25 03:20:53 +0000

    Add constraint to votes table

--- fedora_elections/models.py
+++ fedora_elections/models.py
@@ -85,6 +85,10 @@ class LegalVoter(db.Model):
 class Vote(db.Model):
     __tablename__ = 'votes'
 
+    __table_args__ = (db.UniqueConstraint('election_id', 'voter',
+                                          'candidate_id',
+                                          name='eid_voter_cid'), {} )
+
     id = db.Column(db.Integer, primary_key=True)
     election_id = db.Column(db.Integer, db.ForeignKey('elections.id'),
                             nullable=False)
@@ -97,3 +101,4 @@ class Vote(db.Model):
     candidate = db.relationship('Candidate', backref=db.backref('votes',
                                                                 lazy='dynamic'))
     value = db.Column(db.Integer, nullable=False)
+
