51056537b3856b5cf548a40d69fd9762c0f00734
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-02-19 15:13:25 +0100

    Add script to easily create the database required to work on elections

---
+++ createdb.py
@@ -0,0 +1,7 @@
+import __main__
+__main__.__requires__ = ['SQLAlchemy >= 0.7']
+import pkg_resources
+
+from fedora_elections import APP
+from fedora_elections.models import create_tables
+create_tables(APP.config['DB_URL'], debug=True)
