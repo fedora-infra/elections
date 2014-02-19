01651e69b77d541f417f00cbc6ebcc8942cffa39
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-02-19 13:21:54 +0100

    Populate the default config with the values present in the sample development configuration file

--- fedora_elections/default_config.py
+++ fedora_elections/default_config.py
@@ -1,1 +1,18 @@
 FEDORA_ELECTIONS_ADMIN_GROUP = 'elections'
+
+# url to the database server:
+DB_URL = 'sqlite:////var/tmp/elections_dev.sqlite'
+
+
+# You will want to change this for your install
+SECRET_KEY = 'change me'
+
+FAS_BASE_URL = 'https://fakefas.fedoraproject.org/accounts/'
+FAS_USERNAME = 'elections'
+FAS_PASSWORD ='el3ctMEn0w'
+FAS_CHECK_CERT = False
+FAS_HTTPS_REQUIRED = False
+
+FEDORA_ELECTIONS_ADMIN_GROUP = 'elections'
+# List of URLs for additional site-wide CSS
+EXTRA_CSS = []
