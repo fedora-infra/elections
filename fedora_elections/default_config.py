0e9dcb4ddde797d2b0d6d0a8591a09981841bc3d
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-03-27 16:34:23 +0100

    Clean configuration file
    - Change from fakefas to stg
    - Remove username
    - Remove password
    - Remove unused configuration key

--- fedora_elections/default_config.py
+++ fedora_elections/default_config.py
@@ -7,11 +7,10 @@ DB_URL = 'sqlite:////var/tmp/elections_dev.sqlite'
 # You will want to change this for your install
 SECRET_KEY = 'change me'
 
-FAS_BASE_URL = 'https://fakefas.fedoraproject.org/accounts/'
-FAS_USERNAME = 'elections'
-FAS_PASSWORD = 'el3ctMEn0w'
+FAS_BASE_URL = 'https://admin.stg.fedoraproject.org/accounts/'
+FAS_USERNAME = ''
+FAS_PASSWORD = ''
 FAS_CHECK_CERT = False
-FAS_HTTPS_REQUIRED = False
 
 FEDORA_ELECTIONS_ADMIN_GROUP = 'elections'
 # List of URLs for additional site-wide CSS
