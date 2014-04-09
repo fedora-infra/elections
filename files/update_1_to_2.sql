e22989e22bda006680fa66120aa0f8525f74e07f
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-04-09 09:55:33 +0200

    Add sql upgrade script, allowing to convert the database from elections v1 to v2

---
+++ files/update_1_to_2.sql
@@ -0,0 +1,17 @@
+-- This script is used to convert an elections database from the version 0.x
+-- to the version 2.x.
+-- There was some changes made to the model during the rewrite to flask (v2.x)
+-- and this allows to convert from v1 to v2.
+
+ALTER TABLE elections ADD COLUMN voting_type VARCHAR(100) NOT NULL DEFAULT 'range';
+
+ALTER TABLE elections ADD COLUMN candidates_are_fasusers BOOLEAN NOT NULL DEFAULT false;
+
+ALTER TABLE elections ADD COLUMN fas_user VARCHAR(50);
+
+ALTER TABLE votes RENAME COLUMN weight TO value;
+
+ALTER TABLE elections DROP COLUMN votes_per_user;
+
+
+-- ALTER TABLE elections RENAME COLUMN shortdesc TO summary;
