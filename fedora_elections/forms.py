8485b0d3b077be45290ca07e4fafeef3480b62b4
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-04-09 08:25:34 +0200

    Rename the field `election.summary` into `election.shortdesc`
    shortdesc is the field used in the elections 0.x, let's keep using it to
    reduce database change when updating to 2.x

--- fedora_elections/forms.py
+++ fedora_elections/forms.py
@@ -6,7 +6,7 @@ from fedora_elections.models import Election
 
 
 class ElectionForm(wtf.Form):
-    summary = wtforms.TextField(
+    shortdesc = wtforms.TextField(
         'Summary<span class="error">*</span>', [
             wtforms.validators.Required(),
             wtforms.validators.Length(max=150)])
@@ -61,7 +61,7 @@ class ElectionForm(wtf.Form):
         form._election_id = election_id
 
     def validate_summary(form, field):
-        check = Election.search(SESSION, summary=form.summary.data)
+        check = Election.search(SESSION, shortdesc=form.shortdesc.data)
         if check:
             if not (form._election_id and form._election_id == check[0].id):
                 raise wtforms.ValidationError(
