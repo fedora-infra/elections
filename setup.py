0aef5a4875a60fe8d4c371a9794c436989753a78
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-05-02 17:49:19 +0200

    pep8 fixes on setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 Setup script
 """
 
-## These two lines are needed to run on EL6
+# These two lines are needed to run on EL6
 __requires__ = ['SQLAlchemy >= 0.7', 'jinja2 >= 2.4']
 import pkg_resources
 
