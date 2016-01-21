b134c1f22ac3d56b2ea13c713f91dd7fd0a416d2
Pierre-Yves Chibon <pingou@pingoured.fr>
2016-01-21 16:50:13 +0100

    Drop get_fas_human_name since we store the info in the DB

--- fedora_elections/utils.py
+++ fedora_elections/utils.py
@@ -11,20 +11,6 @@ def build_name_map(election):
         return {}
 
     return dict([
-        (str(candidate.id), get_fas_human_name(candidate.name))
+        (str(candidate.id), candidate.fas_name)
         for candidate in election.candidates
     ])
-
-
-@fedora_elections.cache.cache_on_arguments()
-def get_fas_human_name(username):
-    """ Given a fas username, return the fas human_name if possible.
-
-    If the user has their name set to private or they don't exist, we just
-    return the given username as a stand-in.
-    """
-    try:
-        return fedora_elections.FAS2.person_by_username(username)['human_name']
-    except (KeyError, AuthError), err:
-        fedora_elections.APP.logger.debug(err)
-        return username
