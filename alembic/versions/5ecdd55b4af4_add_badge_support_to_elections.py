ea85cd17dc2161e932805bff3cbfd687a0e624b6
Pierre-Yves Chibon <pingou@pingoured.fr>
2019-03-18 13:13:32 +0100

    Add an alembic migration script for the voted_badge field in elections
    Signed-off-by: Pierre-Yves Chibon <pingou@pingoured.fr>

---
+++ alembic/versions/5ecdd55b4af4_add_badge_support_to_elections.py
@@ -0,0 +1,27 @@
+"""Add badge support to elections
+
+Revision ID: 5ecdd55b4af4
+Revises: 2b8f5a6f10a4
+Create Date: 2019-03-18 12:21:59.536380
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '5ecdd55b4af4'
+down_revision = '2b8f5a6f10a4'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    """ Add the voted_badge column to the Elections table. """
+    op.add_column(
+        'elections',
+        sa.Column('voted_badge', sa.Unicode(250), nullable=True)
+    )
+
+
+def downgrade():
+    """ Drop the voted_badge column from the Elections table. """
+    op.drop_column('elections', 'voted_badge')
