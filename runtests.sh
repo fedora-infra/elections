442f9b9b7f19d11f515932d36f74fc73eee83706
Pierre-Yves Chibon <pingou@pingoured.fr>
2019-05-02 16:21:32 +0200

    Move the tests to run on py3 by default
    Signed-off-by: Pierre-Yves Chibon <pingou@pingoured.fr>

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 FEDORA_ELECTIONS_CONFIG=`pwd`/tests/config \
-PYTHONPATH=fedora_elections ./nosetests \
+PYTHONPATH=fedora_elections python3 ./nosetests \
 --with-coverage --cover-erase --cover-package=fedora_elections $*
