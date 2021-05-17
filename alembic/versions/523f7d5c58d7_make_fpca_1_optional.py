4b0bf7d2b80cd51bba69bd533ccafafa15ff4864
Ben Cotton <bcotton@fedoraproject.org>
2021-05-17 12:56:51 -0400

    Fix syntax error in alembic migration

--- alembic/versions/523f7d5c58d7_make_fpca_1_optional.py
+++ alembic/versions/523f7d5c58d7_make_fpca_1_optional.py
@@ -17,12 +17,12 @@ import sqlalchemy as sa
 def upgrade():
     """Add the FPCA+1 requirement option to elections"""
     op.add_column(
-            'requires_plusone',
-            sa.Column(sa.Boolean, default=False)
+            "elections",
+            sa.Column("requires_plusone", sa.Boolean, default=False)
     )
 
 def downgrade():
     """Drop the FPCA+1 requirement option"""
     op.remove_column(
-            'requires_plusone'
+            "elections", "requires_plusone"
     )
